e2744764bf63ae7acf10d7a818cbf4b8c9d69204
Arslan <ars.annaklychev@gmail.com>
2026-04-22 20:08:21 +0300

    Добавил таблицу о выполнении привычек

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, DateTime, Boolean, Time, ForeignKey
+from sqlalchemy import Column, Integer, String, DateTime, Boolean, Time, ForeignKey, Date
 from .database import Base
 import datetime
 from sqlalchemy.sql import func
@@ -20,5 +20,13 @@ class Habit(Base):
     reminder_time = Column(Time, nullable=True)
     days_of_week = Column(String, nullable=True)
     created_at = Column(DateTime, server_default=func.now())
+    
+class HabitCompletion(Base):
+    __tablename__ = "habit_completions"
+    id = Column(Integer, primary_key=True, index=True)
+    habit_id = Column(Integer, ForeignKey("habits.id"), nullable=False)
+    date = Column(Date, nullable=False)
+    completed = Column(Boolean, default=False)
+    points_earned = Column(Integer, default=0)
     
     
